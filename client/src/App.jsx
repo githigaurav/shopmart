a404aa3274aeedc36c29a38c127f16c2305bb0a9
Gaurav Kumar <gfxshaolin@gmail.com>
2024-01-03 15:12:17 +0530

    React router dom && Js-Cookie && tailwind configured and ReadMe.md added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
 const App = () => {
   return (
-    <div>Initial Stage</div>
+   <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<h1>Routes Configured</h1>}/>
+        </Routes>
+      </Router>
+   </>
   )
 }
 
