bb95a3f813aab111bd1490e41e4c3e1a0d88a329
Gaurav Kumar <gfxshaolin@gmail.com>
2024-01-03 15:24:57 +0530

    nodemon express added and server setup

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+### Getting started
+1. Client Side 
+ ```cd client```
+ ```npm install```
+ ```npm run dev```
+.
+
+
+2. Server Side 
+    ```cd server```
+    ```npm install```
+    ```npm start```
---
+++ server/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config()
+const express = require("express")
+const server= express()
+
+
+
+
+
+
+
+
+
+
+
+const PORT=process.env.PORT
+server.listen(PORT,()=>{
+    console.log(`Server Status:OK  PORT ${PORT} `)
+})
