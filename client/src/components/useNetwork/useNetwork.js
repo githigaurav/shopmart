685a4bb94cff3067898fd1e4720c51ab5e9668ed
Gaurav Kumar <gfxshaolin@gmail.com>
2024-01-05 12:35:36 +0530

    network request updated

--- client/src/components/useNetwork/useNetwork.js
+++ client/src/components/useNetwork/useNetwork.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
-
-const networkPath=window.location.host === 'local' ? "http://localhost:3001" : "https://shopmart-server.vercel.app"
+const networkPath=window.location.host === 'localhost:3000' ? "http://localhost:3001" : "https://shopmart-server.vercel.app"
 export const useAxios = async (
     type,
     networkURL,
